a120058837d4ecd02a2c4468fba0f68d25ff4ded
Ian Utz <iautz@ursinus.edu>
2023-09-03 15:52:10 -0400

    Traverse through states of String

---
+++ HW1.py
@@ -0,0 +1,26 @@
+import random
+
+next = {} # This will hold the directed graph
+s = "Welcome to cs 477"
+# This loops through all of the characters in s
+# and keeps track of their indices
+for i, c in enumerate(s): 
+    if not c in next:
+        # If this is the first time seeing a particular 
+        # character, we need to make a new key/value pair for it
+        next[c] = []
+    if i < len(s)-1: # If there is a character after this
+        # Record that s[i+1] is one of the following characters
+        next[c].append(s[i+1])
+
+res = "W"
+max_len = len(s)
+while len(res) != max_len:
+    nex_arr = next[res[-1]]
+    if len(nex_arr) > 0:
+        rand_char = random.randint(0, len(nex_arr)-1)
+        res += nex_arr[rand_char]
+    else:
+        break
+        
+print(res)
